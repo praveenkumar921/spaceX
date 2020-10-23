bff9b47e5efd9d7e179cf9926e4df98c3b22aa67
praveenkumar921 <praveen.kumar921@gmail.com>
2020-10-23 13:20:29 +0530

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,37 +8,31 @@ import { MyServiceService } from './my-service.service';
 })
 export class AppComponent implements OnInit {
   public mArticles: Array<any>;
-  title = 'SpaceX';
   constructor(public mySerive: MyServiceService) {
-
   }
 
 
   ngOnInit() {
     this.mySerive.topStories().subscribe((data: any) => {
-      this.mArticles = data,
-        console.log('data', data);
+      this.mArticles = data;
     });
   }
 
   public getByYear(year) {
     this.mySerive.getByYear(year).subscribe((data: any) => {
-      this.mArticles = data,
-        console.log('data', data);
+      this.mArticles = data;
     });
   }
 
   public getByLaunch(flag) {
     this.mySerive.getByLaunch(flag).subscribe((data: any) => {
-      this.mArticles = data,
-        console.log('data', data);
+      this.mArticles = data;
     });
   }
 
   public getByLanding(flag) {
     this.mySerive.getByLanding(flag).subscribe((data: any) => {
-      this.mArticles = data,
-        console.log('data', data);
+      this.mArticles = data;
     });
   }
 }
